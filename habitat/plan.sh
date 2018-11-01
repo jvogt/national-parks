3ff50045dab2aaa05fa09f11202682c7d7c1775d
Jeff Vogt <jvogt@chef.io>
2018-11-01 08:58:53 -0700

    bump version

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=national-parks
 pkg_description="A sample JavaEE Web app deployed in the Tomcat8 package"
 pkg_origin=jvogt
-pkg_version=7.1.0
+pkg_version=7.2.0
 pkg_maintainer="Bill Meyer <bill@chef.io>"
 pkg_license=('Apache-2.0')
 pkg_deps=(core/tomcat8 core/jre8)
 pkg_build_deps=(core/jdk8/8u131 core/maven)
 pkg_svc_user="root"
-pkg_binds=(
+pkg_binds_optional=(
   [database]="port"
 )
 pkg_exports=(
